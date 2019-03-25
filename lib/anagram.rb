16320eba198e13d17f33cbaf90cdf7734f950c76
Dr. XyClo <sdcarmody@gmail.com>
2019-03-25 01:47:53 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -16,12 +16,14 @@ class Anagram
     anagram_array.select do |word|
       if @word == word
         @@words << word
-        
       end 
     end 
   end 
   
-  
+  # take initialzied word and split into an array
+  # take the incoming array word and split into an array
+  # compare the two arrays
+  # if the two arrays are equal return the incoming array word
   
 end 
 
