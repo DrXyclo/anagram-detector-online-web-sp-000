67377831c12a6bd7677852bd00b4d1abe109e0c5
Dr. XyClo <sdcarmody@gmail.com>
2019-03-25 00:57:09 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
 # Your code goes here!
+
+require 'pry'
+
+class Anagram
+  attr_accessor :word 
+  
+  
+  def initialize(word)
+    binding.pry
+    @word = word 
+    binding.pry 
+  end 
+  
+  
+  
+  
+  
+end 
+
+=begin 
+  it 'should detect no matches' do
+    diaper = Anagram.new('diaper')
+    expect(diaper.match(%w(hello world zombies pants dipper))).to eq([])
+  end
+=end 
